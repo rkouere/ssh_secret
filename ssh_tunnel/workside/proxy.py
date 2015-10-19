f9ba54a900629c6f79b38af3bf8bf68f93f2dfb8
calve <calvinh34@gmail.com>
2015-10-20 00:49:07 +0200

    Powerfull filtering proxy

--- ssh_tunnel/workside/proxy.py
+++ ssh_tunnel/workside/proxy.py
@@ -3,16 +3,65 @@ import argparse
 import shutil
 import io
 import binascii
+import logging
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-blacklisted = set()
+
+blacklisted_uris = set()
 
 
-class BlacklistedException(Exception):
-    pass
+def blacklist(path):
+    blacklisted_uris.add(path)
+    logging.info("Added {} to the blacklist".format(path))
+    logging.info("Blacklist : {}".format(blacklisted_uris))
+
+
+class Filter():
+    """Abstract class which defines a Filter plugin. It must implement the ``drop`` method"""
+    def drop(self, path, headers, body):
+        """Returns True if the request is suspitious and should be filtered"""
+        raise Exception("Should be implemented")
+
+
+class OpenSSHStringFilter(Filter):
+    """Finds the OpenSSH version exchange at the beginnig of the protocol"""
+    def drop(self, path, headers, body):
+        bodies = []
+        # Construct a list of decoded bodies
+        bodies.append(body)
+        try:
+            bodies.append(binascii.a2b_base64(body))
+        except binascii.Error:
+            print("not a base64")
+
+        for target in bodies:
+            if len(target) < 32 and b"OpenSSH" in target:
+                print("Openssh detected")
+                blacklist(path)
+                return True
+        return False
+
+
+class BlacklistFilter(Filter):
+    def drop(self, path, headers, body):
+        """Drops a request if the uri is in a global blacklist"""
+        return path in blacklisted_uris
+
+
+class UserAgentFilter(Filter):
+    """Filter illegitimate User Agent"""
+    def drop(self, path, headers, body):
+        return "mozilla" not in headers.get('User-Agent', '').lower()
 
 
 class ProxyHandler(BaseHTTPRequestHandler):
+
+    filters = [
+        BlacklistFilter(),
+        OpenSSHStringFilter(),
+#        UserAgentFilter()
+    ]
+
     def do_GET(self):
         try:
             request = requests.get(self.path)
@@ -28,53 +77,34 @@ class ProxyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length', 0))
         body = self.rfile.read(content_len)
-        try:
-            self.filter_request(body)
-        except BlacklistedException:
-            self.send_response(400)
-            self.end_headers()
-            self.log_message("Suspicious behaviour detected at {}".format(self.path))
+        if self.filter_request(body):
+            return
         try:
             request = requests.post(self.path, data=body)
-            self.filter_request(request.content)
+            if self.filter_request(request.content, path=self.path, headers=request.headers):
+                return
             f = io.BytesIO()
             f.write(request.content)
             f.seek(0)
             self.send_response(request.status_code)
             self.end_headers()
             shutil.copyfileobj(f, self.wfile)
-        except (ConnectionRefusedError, requests.exceptions.ConnectTimeout):
+        except (ConnectionRefusedError, requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
             self.log_message("{} not reachable".format(self.path))
-        except BlacklistedException:
-            self.send_response(400)
-            self.end_headers()
-            self.log_message("Suspicious behaviour detected at {}".format(self.path))
 
-    def filter_request(self, body):
+    def filter_request(self, body, path=None, headers=None):
         """
-        Return True if the request should be filtered
+        Respond with 400 if a request is suspicious
         """
-        bodies = []
-        if self.path in blacklisted:
-            self.log_message("{} is blacklisted".format(self.path))
-            raise BlacklistedException()
-        # Construct a list of decoded bodies
-        bodies.append(body)
-        try:
-            bodies.append(binascii.a2b_base64(body))
-        except binascii.Error:
-            self.log_message("Not a base64")
-
-        for target in bodies:
-            if len(target) < 32 and b"OpenSSH" in target:
-                self.log_message("Openssh detected")
-                self.blacklist(self.path)
-                raise BlacklistedException()
-
-    def blacklist(self, host):
-        blacklisted.add(host)
-        self.log_message("Added {} to the blacklist".format(self.path))
-        self.log_message("Blacklist : {}".format(blacklisted))
+        if not headers:
+            headers = self.headers
+        if not path:
+            path = self.path
+        for f in self.filters:
+            if f.drop(self.path, self.headers, body):
+                self.send_response(400)
+                self.end_headers()
+                self.log_message("Suspicious behaviour detected at {} by {}".format(self.path, f))
 
 
 if __name__ == '__main__':

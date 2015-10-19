fdc95012b8e0872f5978305be563d48dc61124ea
calve <calvinh34@gmail.com>
2015-10-20 00:49:56 +0200

    Nice program tester

--- ssh_tunnel/test/test_global.py
+++ ssh_tunnel/test/test_global.py
@@ -1,7 +1,24 @@
-import unittest
+import time
+from threading import Thread
 from ssh_tunnel.workside.workside import run as work_run
 from ssh_tunnel.homeside.homeside import run as home_run
 
+
+class AbstractProgram(Thread):
+    def __init__(self, passphrase, runner, *args, **kwargs):
+        self.passphrase = passphrase
+        self.runner = runner
+        super(*args, **kwargs)
+        Thread.__init__(self)
+
+    def run(self):
+        self.runner(self.passphrase)
+
+
 passphrase = "plop"
-work_run(passphrase)
-home_run(passphrase)
+work_thread = AbstractProgram(passphrase, work_run)
+home_thread = AbstractProgram(passphrase, home_run)
+
+work_thread.start()
+time.sleep(2)
+home_thread.start()

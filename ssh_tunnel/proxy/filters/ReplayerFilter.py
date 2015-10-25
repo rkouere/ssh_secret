20db0922319f0c64a5360c32d7baa5007f8e22a8
calve <calvinh34@gmail.com>
2015-10-25 22:56:54 +0100

    Better rate for ReplayerFilter

--- ssh_tunnel/proxy/filters/ReplayerFilter.py
+++ ssh_tunnel/proxy/filters/ReplayerFilter.py
@@ -7,7 +7,7 @@ from ssh_tunnel.proxy.filters import Filter
 class ReplayerFilter(Filter):
     """Technically not a filter ; randomly replay requests to mess with the servers"""
     def drop(self, path, headers, body):
-        if random.getrandbits(2) > 3:
+        if random.randint(0, 100) > 70:
             logging.info("replaying request for the lulz")
             try:
                 requests.post(path, headers=headers, data=body)

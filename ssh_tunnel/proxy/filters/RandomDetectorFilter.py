f7e5d9f6fcafb23ccbc4c42758de02e53a39175c
calve <calvinh34@gmail.com>
2015-10-25 22:57:10 +0100

    Factorise RandomDetectorFilter

--- ssh_tunnel/proxy/filters/RandomDetectorFilter.py
+++ ssh_tunnel/proxy/filters/RandomDetectorFilter.py
@@ -2,33 +2,32 @@ from urllib.parse import urlparse
 import logging
 from ssh_tunnel.proxy.filters import Filter
 
+RANDOM_THRESHOLD = 0.87
+rate_by_host = {}
+
+
+def random_for_host(host):
+    return (rate_by_host[host]['rate'] / rate_by_host[host]['total'])
+
+
+def random_factor(body):
+    count = 0
+    for b in bytearray(body):
+        if 0x20 <= b <= 0xfe:
+            count += 1
+    return count
+
 
 class RandomDetectorFilter(Filter):
     """Drop requests if they contain too much random data"""
-    total_random = 0
-    total_body = 0
-    rate_by_host = {}
-    RANDOM_THRESHOLD = 0.80
-    # Install a custom error handler for decode()
-
-    def random_factor(self, body):
-        count = 0
-        for b in bytearray(body):
-            if 0x20 <= b <= 0xfe:
-                count += 1
-        return count
-
-    def random_for_host(self, host):
-        return (self.rate_by_host[host]['rate'] / self.rate_by_host[host]['total'])
-
     def drop(self, path, headers, body):
         if len(body):
-            rate = self.random_factor(body)
+            rate = random_factor(body)
             host = urlparse(path).netloc
-            if host not in self.rate_by_host:
-                self.rate_by_host[host] = {'rate': 0, 'total': 0, 'count': 0}
-            self.rate_by_host[host]['rate'] += rate
-            self.rate_by_host[host]['total'] += len(body)
-            self.rate_by_host[host]['count'] += 1
-            logging.info("Random rate : {} (at {})".format(self.random_for_host(host), path))
-            return self.rate_by_host[host]['count'] > 2 and self.random_for_host(host) < self.RANDOM_THRESHOLD
+            if host not in rate_by_host:
+                rate_by_host[host] = {'rate': 0, 'total': 0, 'count': 0}
+            rate_by_host[host]['rate'] += rate
+            rate_by_host[host]['total'] += len(body)
+            rate_by_host[host]['count'] += 1
+            logging.info("Random rate : {} (host {})".format(random_for_host(host), host))
+            return rate_by_host[host]['count'] > 2 and random_for_host(host) < RANDOM_THRESHOLD

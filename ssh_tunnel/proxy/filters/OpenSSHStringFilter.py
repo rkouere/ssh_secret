134f68e27a6613913b748d2b1dcbc2bd2267388a
rkouere <rkouere@gmail.com>
2015-10-25 19:56:51 +0100

    changement verification presence ssh avec wildcards

--- ssh_tunnel/proxy/filters/OpenSSHStringFilter.py
+++ ssh_tunnel/proxy/filters/OpenSSHStringFilter.py
@@ -1,21 +1,33 @@
 import binascii
 import logging
 from ssh_tunnel.proxy.filters import blacklist, Filter
+import re
 
 
 class OpenSSHStringFilter(Filter):
-    """Finds the OpenSSH version exchange at the begining of the protocol"""
+    """
+    Finds the OpenSSH version exchange at the begining of the protocol
+    Info : according to the ssh rfc4253:
+    "When the connection has been established, both sides MUST send an
+    identification string.  This identification string MUST be
+             SSH-protoversion-softwareversion
+    Let's regex this...
+    """
     def drop(self, path, headers, body):
+        prog = re.compile('^SSH-[0-9]+(\.)?[0-9]?-(.*?) ')
         bodies = []
         # Construct a list of decoded bodies
         bodies.append(body)
         try:
             bodies.append(binascii.a2b_base64(body))
+            print("=======================")
+            print(bodies)
         except binascii.Error:
             logging.debug("not a base64")
 
         for target in bodies:
-            if len(target) < 32 and b"OpenSSH" in target:
+            print("length targer = " + str(len(target)))
+            if prog.match(target.decode()):
                 logging.info("Openssh detected")
                 blacklist(path)
                 return True
